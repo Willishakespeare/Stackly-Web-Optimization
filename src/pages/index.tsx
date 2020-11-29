a5ba04b6bfc4448f0cc0e623b5ba55dab59e2101
Willishakespeare <Skr13@outlook.com>
2020-11-28 23:26:30 -0700

    1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import dynamic from "next/dynamic";
-import OrganismsNavigation from "@Organisms/navegation";
+import TemplateMain from "@Templates/index";
+import OrganismHero from "@Organisms/hero";
+import OrganismServices from "@Organisms/services";
+import OrganismProjects from "@Organisms/projects";
+import OrganismClients from "@Organisms/clients";
+import OrganismContact from "@Organisms/contact";
 import Seo from "@Utils/seo";
 import I18n from "@Src/i18n";
 import { TFunction } from "next-i18next";
@@ -67,40 +71,31 @@ type Props = {
 };
 
 const PageHome = ({ t }: Props) => {
-  const OrganismHero = dynamic(() => import("@Organisms/hero"));
-  const OrganismServices = dynamic(() => import("@Organisms/services"));
-  const OrganismProjects = dynamic(() => import("@Organisms/projects"));
-  const OrganismClients = dynamic(() => import("@Organisms/clients"));
-  const OrganismContact = dynamic(() => import("@Organisms/contact"));
-  const OrganismFooter = dynamic(() => import("@Organisms/footer"));
-
   return (
     <>
       <Seo
         page="Home"
         description="In Stackly Code we understand that time is the most valuable asset and if you put it in our hands, we will make sure to bring the best product for your needs."
       />
-      <OrganismsNavigation t={t} />
-      <OrganismHero t={t} idScroll="HeroScroll" />
-      <LazyLoad debounce>
-        <OrganismServices t={t} idScroll="ServicesScroll" />
-      </LazyLoad>
-      <LazyLoad debounce>
-        <OrganismProjects
-          t={t}
-          idScroll="ProjectsScroll"
-          projects={ProjectsDataFake}
-        />
-      </LazyLoad>
-      <LazyLoad debounce>
-        <OrganismClients t={t} idScroll="ClientsScroll" />
-      </LazyLoad>
-      <LazyLoad debounce>
-        <OrganismContact t={t} idScroll="ContactScroll" />
-      </LazyLoad>
-      <LazyLoad debounce>
-        <OrganismFooter t={t} />
-      </LazyLoad>
+      <TemplateMain t={t}>
+        <OrganismHero t={t} idScroll="HeroScroll" />
+        <LazyLoad>
+          <OrganismServices t={t} idScroll="ServicesScroll" />
+        </LazyLoad>
+        <LazyLoad>
+          <OrganismProjects
+            t={t}
+            idScroll="ProjectsScroll"
+            projects={ProjectsDataFake}
+          />
+        </LazyLoad>
+        <LazyLoad>
+          <OrganismClients t={t} idScroll="ClientsScroll" />
+        </LazyLoad>
+        <LazyLoad>
+          <OrganismContact t={t} idScroll="ContactScroll" />
+        </LazyLoad>
+      </TemplateMain>
     </>
   );
 };

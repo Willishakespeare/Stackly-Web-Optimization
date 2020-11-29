da71aad6afb223367391e2e9d32dc6c15f03bbef
Willishakespeare <Skr13@outlook.com>
2020-11-28 23:49:27 -0700

    1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import OrganismsNavigation from "@Organisms/navegation";
 import Seo from "@Utils/seo";
 import { TFunction } from "next-i18next";
 import I18n from "@Src/i18n";
+import OrganismHero from "@Src/components/organisms/hero";
 
 type Props = {
   t?: TFunction;
@@ -16,6 +17,7 @@ const PageHome = ({ t }: Props) => {
         description="In Stackly Code we understand that time is the most valuable asset and if you put it in our hands, we will make sure to bring the best product for your needs."
       />
       <OrganismsNavigation t={t} />
+      <OrganismHero t={t} />
     </>
   );
 };

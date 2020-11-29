2e3fd0f3547328c532e4d0a0182c81f3e4187aa7
Willishakespeare <Skr13@outlook.com>
2020-11-29 01:34:46 -0700

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -75,6 +75,7 @@ const PageHome = ({ t }: Props) => {
   return (
     <>
       <Seo
+        title="Stackly Code"
         page="Home"
         description="In Stackly Code we understand that time is the most valuable asset and if you put it in our hands, we will make sure to bring the best product for your needs."
       />
